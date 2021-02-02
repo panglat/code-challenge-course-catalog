a42fa3ae4eb546f3d53190549c4e7149d490be70
Pablo Anglat <pablo.anglat@rootstrap.com>
2021-02-02 11:59:58 -0300

    Add favorite star

--- src/components/CourseItem/index.js
+++ src/components/CourseItem/index.js
@@ -1,3 +1,4 @@
+import starFilledImg from 'assets/svg/star-filled.svg';
 import './styles.scss';
 
 function CourseItem({ course }) {
@@ -6,13 +7,20 @@ function CourseItem({ course }) {
       <img
         alt="Instructor Headshot"
         src={course.instructor_image_url}
-        className="course-item__image"
+        className="course-item__instructor-image"
       />
-      <div className="course-item__description">
-        <div className="course-item__instructor-name">
-          {course.instructor_name}
+      <div className="course-item__course-description">
+        <div className="course-item__course-description-text">
+          <div className="course-item__instructor-name">
+            {course.instructor_name}
+          </div>
+          <div className="course-item__course-name">{course.title}</div>
         </div>
-        <div className="course-item__course-name">{course.title}</div>
+        <img
+          className="course-item__course-description-favorite-img"
+          alt="Favorite"
+          src={starFilledImg}
+        />
       </div>
     </div>
   );
